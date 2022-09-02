6c4a8750af8354d23902236a1daf0ce3ae356180
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2022-09-02 14:45:42 +0700

    fix link bug

--- pages/component/catcard.js
+++ pages/component/catcard.js
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function catcard(props) {
   const { contentData } = props;
   var imgUrl;
-  var linker = "../cat/" + props.id;
-  if (contentData?.image?.url == null) {
+  var linker = "../cat/" + contentData.id;
+  if (contentData.image?.url == null) {
     imgUrl = "/notav.jpg";
   } else {
-    imgUrl = contentData?.image?.url;
+    imgUrl = contentData.image?.url;
   }
   return (
     <Link href={linker}>
